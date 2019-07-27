707070d6fa979bd848ba06917499056ab8709e09
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-07-28 11:08:55 +1200

    Re-add semis

--- src/authors.ts
+++ src/authors.ts
@@ -1,8 +1,8 @@
-import generateAuthors from '@tanem/authors';
-import { promises as fs } from 'fs';
-import path from 'path';
+import generateAuthors from '@tanem/authors'
+import { promises as fs } from 'fs'
+import path from 'path'
 
 export const authors = async () => {
-  const result = await generateAuthors();
-  await fs.writeFile(path.join(process.cwd(), 'AUTHORS'), result);
-};
+  const result = await generateAuthors()
+  await fs.writeFile(path.join(process.cwd(), 'AUTHORS'), result)
+}
--- src/cli.ts
+++ src/cli.ts
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 
-import program from 'commander';
-import { audit } from './audit';
-import { authors } from './authors';
+import program from 'commander'
+import { audit } from './audit'
+import { authors } from './authors'
 
 program
   .command('audit')
   .description('fix security vulnerabilities')
   .action(async () => {
     try {
-      await audit();
+      await audit()
     } catch (error) {
-      console.error(error);
-      process.exit(1);
+      console.error(error)
+      process.exit(1)
     }
-  });
+  })
 
 program
   .command('authors')
   .description('write an AUTHORS file to process.cwd()')
   .action(async () => {
     try {
-      await authors();
+      await authors()
     } catch (error) {
-      console.error(error);
-      process.exit(1);
+      console.error(error)
+      process.exit(1)
     }
-  });
+  })
 
-program.parse(process.argv);
+program.parse(process.argv)
