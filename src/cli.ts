5185cfbb5e287544d386270824d997adb147d82a
Tane Morgan <464864+tanem@users.noreply.github.com>
2019-07-28 16:01:31 +1200

    Add changelog script

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import program from 'commander'
 import { audit } from './audit'
 import { authors } from './authors'
+import { changelog } from './changelog'
 
 program
   .command('audit')
@@ -22,6 +23,18 @@ program
   .action(async () => {
     try {
       await authors()
+    } catch (error) {
+      console.error(error)
+      process.exit(1)
+    }
+  })
+
+program
+  .command('changelog')
+  .description('write a CHANGELOG.md file to process.cwd()')
+  .action(async () => {
+    try {
+      await changelog()
     } catch (error) {
       console.error(error)
       process.exit(1)
