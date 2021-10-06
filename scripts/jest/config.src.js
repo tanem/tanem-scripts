2f4b371da4060abf23cd5c18b09735762be667e0
Tane Morgan <464864+tanem@users.noreply.github.com>
2021-10-07 05:52:21 +1300

    Transform git-remote-origin-url

--- scripts/jest/config.src.js
+++ scripts/jest/config.src.js
@@ -3,11 +3,12 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/*.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   rootDir: process.cwd(),
   roots: ['<rootDir>/test'],
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
   testMatch: ['<rootDir>/test/*.test.ts'],
   testRunner: 'jest-jasmine2',
   transform: { '^.+\\.ts$': 'ts-jest' },
+  transformIgnorePatterns: ['node_modules/(?!git-remote-origin-url)'],
 };
